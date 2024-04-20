e7fecceef7ee6283a36af90490e1d53d22e4f97c
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-04-20 16:19:28 +0530

    App is updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, task: 'Learn React', completed: false },
+    { id: 2, task: 'Build a Todo App', completed: false },
+    { id: 3, task: 'Deploy to Heroku', completed: false }
+  ]);
+
+  const handleComplete = (id) => {
+    setTodos(prevTodos => prevTodos.map(todo => {
+      if (todo.id === id) {
+        return { ...todo, completed: true };
+      }
+      return todo;
+    }));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
