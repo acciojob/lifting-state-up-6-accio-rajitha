5e4bb70fe78be6a91242946e41eacec8c11e03ab
accio-rajitha <rajithasabhavath2019@gmail.com>
2024-12-27 19:50:11 +0530

    code added2

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,13 +4,15 @@ import React from 'react';
 const TodoList = ({ todos, handleComplete }) => {
   return (
     <div>
-      <h2>Child Component</h2>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
             {todo.task}
             {!todo.completed && (
-              <button data-testid={`complete-${todo.id}`} onClick={() => handleComplete(todo.id)}>
+              <button
+                data-testid={`complete-${todo.id}`}
+                onClick={() => handleComplete(todo.id)}
+              >
                 Complete
               </button>
             )}
