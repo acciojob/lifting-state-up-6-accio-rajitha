c3e0e16ff5fddfd659f77145db1221c65054e161
accio-rajitha <rajithasabhavath2019@gmail.com>
2025-01-15 18:18:41 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,35 @@ const App = () => {
   ]);
 
   const handleComplete = (id) => {
+    // Update the todos state by marking the specific todo as completed
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: true } : todo
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
+    </div>
+  );
+};
+
+export default App;
+/*
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, task: 'Learn React', completed: false },
+    { id: 2, task: 'Build a Todo App', completed: false },
+    { id: 3, task: 'Deploy to Heroku', completed: false },
+  ]);
+
+  const handleComplete = (id) => {
     console.log(`handleComplete called with id: ${id}`); 
 
     setTodos((prevTodos) => {
@@ -31,3 +60,4 @@ const App = () => {
 };
 
 export default App;
+*/
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,28 @@
 // TodoList.js
+import React from 'react';
+
+const TodoList = ({ todos, handleComplete }) => {
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.task}
+            {!todo.completed && (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
+/*
 // TodoList.js
 import React from 'react';
 
@@ -25,3 +49,4 @@ const TodoList = ({ todos, handleComplete }) => {
 };
 
 export default TodoList;
+*/
